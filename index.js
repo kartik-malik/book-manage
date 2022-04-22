9fbc05862d1f1b8951d2b133cbcd9ea37c6976bc
kartik <kartikmalikboss@gmail.com>
2022-04-22 21:42:45 +0530

    dep

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
-const port = 8081;
+const port = process.env.PORT || 8081;
 
 const cors = require("cors");
 const errorHandler = require("./middlewares/error");
